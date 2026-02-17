c40600d46c88a728bfad5f9b0ea7b272992fc0c2
ltdrobot <ltdrobot@localizethedocs.org>
2026-02-17 11:46:02 +0000

    Deploying to pages from @ localizethedocs/helix-docs-l10n@a0993a5e9ec9d45d7e089ab63440f30cc7b284d2 🚀

---
+++ ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",    "Development"],
+    ["stable",    "Stable Release"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/helix-docs-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/helix-docs-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/helix-docs-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/helix-docs-l10n"],
+  ]
+};
